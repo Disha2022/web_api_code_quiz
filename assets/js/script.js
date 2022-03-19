d050ffdde9d089b4c0cc21bb6085a1281fed7c26
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-03-19 18:52:58 -0400

    javaScript file update

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const quiz = [
+    {
+        question: "Out of 180 countries, what is Ukraine's corruption ranking in 2021? (lower is better)",
+        answers: ["27th", "122nd", "124th", "136th"],
+        correctAnswer: 2
+    },
+    {
+        question: "Out of 180 countries, what is Russia's corruption ranking in 2021? (lower is better)",
+        answers: ["27th", "122nd", "124th", "136th"],
+        correctAnswer: 4
+    },
+    {
+        question: "Out of 180 countries, what is United State's corruption ranking in 2021? (lower is better)",
+        answers: ["1st", "27th", "124th", "180th"],
+        correctAnswer: 2
+    },
+    {
+        question: "Out of 180 countries, what is Mexico's corruption ranking in 2021? (lower is better)",
+        answers: ["27th", "122nd", "124th", "136th"],
+        correctAnswer: 3
+    },
+]
